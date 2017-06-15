331ffcef7965b2237e1c28f168624bf44c384a10
Roshan <rcroshan63@gmail.com>
2017-06-16 00:41:13 +0545

    implemented javascript calculator

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 
+var inputEntry = document.getElementById('inputEntry');
+
+function pushEntry(object) {
+    var pushed = object.innerHTML;
+    
+    if (pushed == 'AC') {
+        inputEntry.innerHTML = '0';
+    } else if (pushed == '=') {
+        inputEntry.innerHTML = eval(inputEntry.innerHTML);
+    } else {
+        if (inputEntry.innerHTML == '0') {
+            inputEntry.innerHTML = pushed;
+        } else {
+            inputEntry.innerHTML += pushed;
+        }
+    }
+}
